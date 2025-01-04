a351bfb28c86065ac6defc4ee0cdf38cc552001a
Bjørn-Otto Hansen <bjorn.ohansen@gmail.com>
2025-01-04 21:27:05 +0100

    Added support for mouse controls for paddles

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
 @export var speed : int = 200
-@export var controller_scheme : Enums.ControllerScheme = Enums.ControllerScheme.KEYBOARD
+@export var controller_scheme : Enums.ControllerScheme = Enums.ControllerScheme.MOUSE
+@export var vertical_edge_distance : float
+
+@onready var mesh_instance_2d : MeshInstance2D = %MeshInstance2D
 
 var initial_x_position : float
+var min_y_position : float
+var max_y_position : float
 
 func _ready():
 	initial_x_position = position.x
+	
+	var mesh : QuadMesh = mesh_instance_2d.mesh
+	var half_paddle_height = mesh.size.y / 2
+	min_y_position = half_paddle_height + vertical_edge_distance
+	max_y_position = get_viewport_rect().size.y - half_paddle_height - vertical_edge_distance
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
@@ -14,6 +24,7 @@ func _physics_process(delta):
 	_handle_input()
 	move_and_slide()
 	
+	# Fix the times when the ball pushes on the paddles a bit
 	if initial_x_position != position.x:
 		position = Vector2(initial_x_position, position.y)
 
@@ -27,7 +38,8 @@ func _handle_input() -> void:
 			_handle_keyboard_and_gamepad_inputs()
 
 func _handle_mouse_inputs() -> void:
-	pass
+	var mouse_position := get_viewport().get_mouse_position()
+	position = Vector2(position.x, clamp(mouse_position.y, min_y_position, max_y_position))
 
 func _handle_keyboard_and_gamepad_inputs() -> void:
 	if Input.is_action_pressed("up"):
