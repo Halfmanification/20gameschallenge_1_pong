05296759efc3a62fe28120a64aa66eec61b6315a
Bjørn-Otto Hansen <bjorn.ohansen@gmail.com>
2025-01-10 20:20:45 +0100

    Added simple main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var start_button = %StartButton
+@onready var settings_button = %SettingsButton
+@onready var quit_button = %QuitButton
+
+const GAME = preload("res://scenes/game.tscn")
+
+func _ready():
+	start_button.pressed.connect(_on_start_button_pressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_packed(GAME)
+
+func _on_settings_button_pressed():
+	pass
+
+func _on_quit_button_pressed():
+	get_tree().call_deferred("quit")
