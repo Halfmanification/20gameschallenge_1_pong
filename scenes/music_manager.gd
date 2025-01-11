8fc8eed9f6e2027c552ffcc93ffb9742b48db2ab
Bjørn-Otto Hansen <bjorn.ohansen@gmail.com>
2025-01-11 07:28:07 +0100

    Added fading of when pause menu is opened or closed

--- scenes/music_manager.gd
+++ scenes/music_manager.gd
@@ -3,15 +3,17 @@ extends AudioStreamPlayer
 @export var normal_volume_db_offset := -10.0
 @export var muted_volume_db_offset := -20.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	GameSignals.open_pause_menu.connect(_on_open_pause_menu)
 	GameSignals.close_pause_menu.connect(_on_close_pause_menu)
 	volume_db = normal_volume_db_offset
 
 func _on_open_pause_menu():
-	volume_db = muted_volume_db_offset
+	_fade(muted_volume_db_offset, 1)
 
 func _on_close_pause_menu(menu: PauseMenu):
-	print("hello")
-	volume_db = normal_volume_db_offset
+	_fade(normal_volume_db_offset, 1)
+
+func _fade(target_volume: float, duration: float) -> void:
+	var tween := create_tween()
+	tween.tween_property(self, "volume_db", target_volume, duration)
